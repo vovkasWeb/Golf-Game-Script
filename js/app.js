745843f5df68c074cd9c0388a8f8205debd29d88
vovkaWeb <vova77557@gmail.com>
2022-11-09 18:11:32 +0200

    first commit

--- js/app.js
+++ js/app.js
@@ -1,23 +1,22 @@
-const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
 
-function golfScore(par, strokes) {
-  // Змініть код лише під цим рядком
-  if(strokes===1){
-      return names[0];
-  }else if(strokes<= par - 2){
-   return names[1];
-  }else if(strokes== par - 1){
-   return names[2];
-  }else if(strokes== par){
-   return names[3];
-  }else if(strokes== par+1){
-   return names[4];
-  }else if(strokes== par+2){
-   return names[5];
-  }else if(strokes>= par+3){
-   return names[6];
-  }
+let count = 0;
 
-  return "Change Me";
-  // Змініть код лише над цим рядком
+function cc(card) {
+
+if(card===2 || card===3 || card===4 ||card===5 || card===6 ){
+  count++;
 }
+else if(card===10 || card==='J' || card==='Q' ||card==='K'|| card==='A' ){
+ count--;
+}
+if(count>0){
+  return `${count} Bet`;
+}else if(count<=0){
+  return `${count} Hold`;
+}
+return "Change Me";
+
+}
+  
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
